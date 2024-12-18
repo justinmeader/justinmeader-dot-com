cf35cfc0a148da0be4e63f6eceb83e800a27bca2
Justin Meader <jhmeader@gmail.com>
2024-12-18 02:35:03 +0000

    Updated 1 file via CloudCannon.

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "justinmeader dot com",
+  DESCRIPTION: "He is too bald to trust.",
+  AUTHOR: "Justin Meader",
 }
 
 // Work Page
